0c07091f9d9460126a026d08c96b2a9b40429327
shashihacks <shashisoft@outlook.com>
2021-10-24 17:59:23 +0200

    form add in settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -7,17 +8,37 @@ import { AccountService } from '../services/account.service';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  form: FormGroup;
   item: string = 'Profile'
-  constructor(private accountService: AccountService) { }
+  user: Object
+  constructor(private accountService: AccountService, private formBuilder: FormBuilder,) {
+
+
+
+  }
 
   ngOnInit(): void {
     this.accountService.getUserInfo().subscribe(info => {
-      console.log(info)
+      this.user = info
+    })
+
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      firstName: ['', Validators.required],
+
+      lastName: ['', Validators.required],
+
     })
   }
 
 
   changeItem(name) {
     this.item = name
+  }
+  get f() { return this.form.controls; }
+
+  save() {
+
   }
 }
